1e537016b85d56bfe91d690c13598d03677f03bf
lucasstarepravo <lucas.gerkenstarepravo@postgrad.manchester.ac.uk>
2024-02-13 23:35:56 +0000

    adding wendland c2 with no typos!!

--- functions/discrete_operator.py
+++ functions/discrete_operator.py
@@ -69,11 +69,11 @@ def wendland_rbf(neighbours_r, h):
     return float(w_ji)
 
 
-def wendland_rbf_c6(neighbours_r, h):
+def wendland_rbf_c2(neighbours_r, h):
 
     q = neighbours_r/h
 
-    w_ji = ((7/(4*math.pi))*(2*q+1)*4*(1-.5*1))
+    w_ji = ((7/(4*math.pi))*(2*q+1)*(1-.5*q)**4)
     return float(w_ji)
 
 def calc_hp(exp_a, dist_xy, h):
@@ -140,7 +140,7 @@ def calc_abf(neigh_r, neigh_xy, m_power, h):  # Needs to be written
 
     for index, (x_dist, y_dist) in enumerate(neigh_xy):
         row = []
-        rbf = wendland_rbf_c6(neigh_r[index], h)
+        rbf = wendland_rbf_c2(neigh_r[index], h)
         for power_a, power_b in m_power:
             h_a = calc_hp(power_a, x_dist, h)
             h_b = calc_hp(power_b, y_dist, h)
