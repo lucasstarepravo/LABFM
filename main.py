59794c300e968b2e18589ee11766c423980e1ffb
lucasstarepravo <lucas.gerkenstarepravo@postgrad.manchester.ac.uk>
2024-03-18 16:20:53 +0000

    changing main values to run simulation

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import dill as pickle
 
 
 if __name__ == '__main__':
-    total_nodes_list = [10, 20, 50, 10, 20, 50]
-    polynomial_list = [2, 2, 2, 4, 4, 4]
+    total_nodes_list = [50, 100, 200]
+    polynomial_list = [8, 8, 8]
     results = run(total_nodes_list, polynomial_list)
 
 
